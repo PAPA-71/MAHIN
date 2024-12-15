53b33b910d97262658cb4a34e217d48b3fe8ba99
PAPA-71 <137255056+PAPA-71@users.noreply.github.com>
2024-12-15 13:13:14 +0600

    Delete mahin.py

--- mahin.py
+++
@@ -1,9 +0,0 @@
-import platform
-import os
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-    __import__("nothung")._login()
-elif 'aarch' in arc:
-    __import__("MAHIN").licenseone()
-else:
-    exit(f'unknow device machine {arc}')
